f9eda1a3f131562ec9b57b564f80205e25c500be
CarolinaChen123 <46692079+CarolinaChen123@users.noreply.github.com>
2020-08-13 04:22:49 -0400

    Update and rename Problem 1-1.py to Problem_of_1_1.py

---
+++ Chapter 1/Problem_of_1_1.py
@@ -0,0 +1,11 @@
+#Find the transformation matrix that rotates the axis x3 of a rectangular coordinate system 45 degrees toward x1 around the x2 axis.
+#Full solutions may be given later.
+
+import numpy as npy
+from math import sqrt
+
+#A is supposed to be a matrix with 3 rows and 1 column
+#def transformation(A):
+#    Transformation = [[1/sqrt(2), 0, -1/sqrt(2)], [0, 1, 0], [1/sqrt(2), 0, 1/sqrt(2)]]
+#    for i in range(lex(
+#This is not complete and I am going to sleep!
