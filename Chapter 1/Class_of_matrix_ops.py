68963a005193acecac39feee457932b7785a57e9
CarolinaChen123 <46692079+CarolinaChen123@users.noreply.github.com>
2020-08-14 00:16:17 -0400

    Update Class_of_matrix_ops.py

--- Chapter 1/Class_of_matrix_ops.py
+++ Chapter 1/Class_of_matrix_ops.py
@@ -7,27 +7,27 @@ class matrix_op(object):
     __slots__ = ('_choice', '_MatrixA', '_MatrixB')  
   
     def __init__(self, choice, MatrixA, MatrixB):
-        self._choice = choice
-        self._MatrixA = MatrixA
-        self._MatrixB = MatrixB
+        self.choice = choice
+        self.MatrixA = MatrixA
+        self.MatrixB = MatrixB
          
     def addition(self):
-        if choice == 1:
+        if self.choice == 1:
             resultadd = np.add(self.MatrixA, self.MatrixB)
         return resultadd
     
     def substraction(self):
-        if choice == 2:
+        if self.choice == 2:
             resultsub = np.subtract(self.MatrixA, self.MatrixB)
         return resultsub
       
     def dotproduct(self):
-        if choice == 3:
+        if self.choice == 3:
             resultdot = np.dot(self.MatrixA, self.MatrixB)
         return resultdot
     
     def crossproduct(self):
-        if choice == 4:
+        if self.choice == 4:
             resultcross = np.cross(self.MatrixA, self.MatrixB)
         return resultcross
     
