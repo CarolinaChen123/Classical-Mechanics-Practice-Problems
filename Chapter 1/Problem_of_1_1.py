486bbe66df4d86b84a8cae09f5be27728d46f6ce
CarolinaChen123 <46692079+CarolinaChen123@users.noreply.github.com>
2020-08-13 20:13:10 -0400

    Update Problem_of_1_1.py

--- Chapter 1/Problem_of_1_1.py
+++ Chapter 1/Problem_of_1_1.py
@@ -9,8 +9,8 @@ from math import sqrt
 #A is supposed to be a matrix with 3 rows and 1 column
 def transformation(A):
     #Let me just define a 3 by 1 matrix here with all zero elements
-    initials = array([0, 0, 0])
-    Transformation = array([[1/sqrt(2), 0, -1/sqrt(2)], [0, 1, 0], [1/sqrt(2), 0, 1/sqrt(2)]])
+    initials = [0, 0, 0]
+    Transformation = [[1/sqrt(2), 0, -1/sqrt(2)], [0, 1, 0], [1/sqrt(2), 0, 1/sqrt(2)]]
     for i in range(len(Transformation)):
         for j in range(len(A)):
             initials[i] += Transformation[i][j]*A[j]
