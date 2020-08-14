c22d7d1d439de0a866bae82dda46900df82f1686
CarolinaChen123 <46692079+CarolinaChen123@users.noreply.github.com>
2020-08-14 00:10:18 -0400

    Update Class_of_matrix_ops.py

--- Chapter 1/Class_of_matrix_ops.py
+++ Chapter 1/Class_of_matrix_ops.py
@@ -1,33 +1,36 @@
 import numpy as np
 
 #MatrixA and MatrixB should be of the same size and the same dimension. I have assumed both as 3 by 1 or 1 by 3.
+#Please ignore this file. :) I am just practicing knowledge about classes and objects.
 Class matrix_op(object):
 
     __slots__ = ('_choice', '_MatrixA', '_MatrixB')  
   
     def __init__(self, choice, MatrixA, MatrixB):
-        self.choice = choice
-        self.MatrixA = MatrixA
-        self.MatrixB = MatrixB
-        
-    def addition(self, choice, MatrixA, MatrixB):
+        self._choice = choice
+        self._MatrixA = MatrixA
+        self._MatrixB = MatrixB
+         
+    def addition(self):
         if choice == 1:
-            resultadd = np.add(MatrixA, MatrixB)
+            resultadd = np.add(self.MatrixA, self.MatrixB)
         return resultadd
     
-    def substraction(self, choice, MatrixA, MatrixB):
+    def substraction(self):
         if choice == 2:
-            resultsub = np.subtract(MatrixA, MatrixB)
+            resultsub = np.subtract(self.MatrixA, self.MatrixB)
         return resultsub
       
-    def dotproduct(self, choice, MatrixA, MatrixB):
+    def dotproduct(self):
         if choice == 3:
-            resultdot = np.dot(MatrixA, MatrixB)
+            resultdot = np.dot(self.MatrixA, self.MatrixB)
         return resultdot
     
-    def crossproduct(self, choice, MatrixA, MatrixB):
+    def crossproduct(self):
         if choice == 4:
-            resultcross = np.cross(MatrixA, MatrixB)
+            resultcross = np.cross(self.MatrixA, self.MatrixB)
         return resultcross
     
+if __name__ == '__main__':
+    main()
      
