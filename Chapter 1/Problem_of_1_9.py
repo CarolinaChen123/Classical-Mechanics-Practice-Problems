461158b27aded65d55288826f98e3f73cd320f50
CarolinaChen123 <46692079+CarolinaChen123@users.noreply.github.com>
2020-08-13 23:07:53 -0400

    Create Problem_of_1_9.py

---
+++ Chapter 1/Problem_of_1_9.py
@@ -0,0 +1,13 @@
+‘’‘
+A = [1, 2, -1]
+B = [-2, 3, 1]
+
+#I have two vectors。 A is [1, 2, -1] and B is [-2, 3, 1].
+#Problem (a) requires us to find A - B and the norm of it.
+
+def add_matrix(whatever1, whatever2):
+    if len(whatever1) == len(whatever2)：
+’‘’
+
+
+
