308f4a4de01a678d2124b735a476a49ead1f9c5d
CarolinaChen123 <46692079+CarolinaChen123@users.noreply.github.com>
2020-08-14 00:28:59 -0400

    Update Problem_of_1_9.py

--- Chapter 1/Problem_of_1_9.py
+++ Chapter 1/Problem_of_1_9.py
@@ -7,9 +7,7 @@ B = [-2, 3, 1]
 #I have two vectors。 A is [1, 2, -1] and B is [-2, 3, 1].
 #Problem (a) requires us to find A - B and the norm of it.
 
-print(np.add(A, B))
-print(
-
+print(np.subtract(A, B))
 
 
 
