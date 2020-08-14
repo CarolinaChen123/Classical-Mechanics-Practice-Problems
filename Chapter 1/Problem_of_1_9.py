9edc796278b690275d8f523bb48e043b8c619cda
CarolinaChen123 <46692079+CarolinaChen123@users.noreply.github.com>
2020-08-14 00:25:46 -0400

    Update Problem_of_1_9.py

--- Chapter 1/Problem_of_1_9.py
+++ Chapter 1/Problem_of_1_9.py
@@ -1,13 +1,15 @@
-‘’‘
+import numpy as np
+from math import sqrt
+
 A = [1, 2, -1]
 B = [-2, 3, 1]
 
 #I have two vectors。 A is [1, 2, -1] and B is [-2, 3, 1].
 #Problem (a) requires us to find A - B and the norm of it.
 
-def add_matrix(whatever1, whatever2):
-    if len(whatever1) == len(whatever2)：
-’‘’
+print(np.add(A, B))
+print(
+
 
 
 
