03b35f0fd96afe97fc2adaa5cca1559495a5934f
CarolinaChen123 <46692079+CarolinaChen123@users.noreply.github.com>
2020-08-16 02:39:21 -0400

    Update Practice_ODE_04.py

--- Chapter 2/ODE/Practice_ODE_04.py
+++ Chapter 2/ODE/Practice_ODE_04.py
@@ -1,1 +1,14 @@
+# The question comes from the following link, but there is no code provided in that link since it is just purely a math tutorial.
+# https://mathinsight.org/ordinary_differential_equation_introduction_examples
+# The differential equation is dx/dt = 5x-3
+
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+
+def function(x, t):
+    dxdt = 5*x - 3
+    return dxdt
+  
+#Initial condition is that x(2) = 3
 
