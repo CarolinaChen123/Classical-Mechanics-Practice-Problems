c7dd8d1d35f8936350921ac922430bd3c3580b2c
CarolinaChen123 <46692079+CarolinaChen123@users.noreply.github.com>
2020-08-16 00:15:52 -0400

    Update Practice_ODE_03.py

--- Chapter 2/ODE/Practice_ODE_03.py
+++ Chapter 2/ODE/Practice_ODE_03.py
@@ -1,1 +1,26 @@
+# I just randomly think of a first-order ODE.
+# dy/dt = t
 
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+
+# function that returns dy/dt
+def model(y,t):
+    dydt = t
+    return dydt
+
+# initial condition
+y0 = 0
+
+# time points
+t = np.linspace(0,20)
+
+# solve ODE
+y = odeint(model,y0,t)
+
+# plot results
+plt.plot(t,y)
+plt.xlabel('time')
+plt.ylabel('y(t)')
+plt.show()
